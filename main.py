ba63e885e741c9bd11767cb81952a55e24572478
yegor-gondhal <ygondhal@gmail.com>
2026-08-27 11:39:57 -0400

    Update

--- main.py
+++ main.py
@@ -93,30 +93,6 @@ def normal(alpha, powers):
     term = xp.sqrt(term)
     return term * xp.power((2*alpha)/xp.pi, 0.75)
 
-def zero_zero_overlap(alpha, beta, cen_a, cen_b):
-    mu = alpha*beta/(alpha+beta)
-    R2 = xp.square(cen_a - cen_b)
-    return xp.sqrt((xp.pi/(alpha+beta)))*xp.exp(-mu*R2)
-
-def one_zero_overlap(alpha, beta, cen_a, cen_b, term):
-    P = (alpha*cen_a + beta*cen_b)/(alpha+beta)
-    return (P-cen_a)*term
-
-def one_one_overlap(alpha, beta, cen_a, cen_b, term):
-    P = (alpha*cen_a + beta*cen_b)/(alpha+beta)
-    term1 = (P-cen_a)*(P-cen_b) + 1/(2*(alpha+beta))
-    return term1*term
-
-def all_cases(p1, p2, alpha, beta, cen_a, cen_b, term):
-    one_zero_idx = xp.where((p1 == 1) & (p2 == 0))
-    zero_one_idx = xp.where((p1 == 0) & (p2 == 1))
-    one_one_idx = xp.where((p1 == 1) & (p2 == 1))
-
-    term[one_zero_idx] = one_zero_overlap(alpha[one_zero_idx], beta[one_zero_idx], cen_a[one_zero_idx], cen_b[one_zero_idx], term[one_zero_idx])
-    term[zero_one_idx] = one_zero_overlap(beta[zero_one_idx], alpha[zero_one_idx], cen_b[zero_one_idx], cen_a[zero_one_idx], term[zero_one_idx])
-    term[one_one_idx] = one_one_overlap(alpha[one_one_idx], beta[one_one_idx], cen_a[one_one_idx], cen_b[one_one_idx], term[one_one_idx])
-    return term
-
 def overlap_1d(alpha, beta, cen_a, cen_b, p1, p2):
     p = xp.add.outer(alpha, beta)
     P = xp.add.outer(alpha*cen_a, beta*cen_b)/p
@@ -157,22 +133,6 @@ def overlap_1d(alpha, beta, cen_a, cen_b, p1, p2):
     int_matrix = xp.sum(int_matrix, axis=-1)
     return int_matrix*outer_coeff
 
-def total_overlap(alpha, beta, cen_a, cen_b, power_a, power_b):
-    term1 = zero_zero_overlap(alpha, beta, cen_a[:, :, 0], cen_b[:, :, 0])
-    term2 = zero_zero_overlap(alpha, beta, cen_a[:, :, 1], cen_b[:, :, 1])
-    term3 = zero_zero_overlap(alpha, beta, cen_a[:, :, 2], cen_b[:, :, 2])
-
-    term1 = all_cases(power_a[:, :, 0], power_b[:, :, 0], alpha, beta, cen_a[:, :, 0], cen_b[:, :, 0], term1)
-    term2 = all_cases(power_a[:, :, 1], power_b[:, :, 1], alpha, beta, cen_a[:, :, 1], cen_b[:, :, 1], term2)
-    term3 = all_cases(power_a[:, :, 2], power_b[:, :, 2], alpha, beta, cen_a[:, :, 2], cen_b[:, :, 2], term3)
-    return term1*term2*term3
-
-def total_overlap1(alpha, beta, cen_a, cen_b, power_a, power_b):
-    term1 = zero_zero_overlap(alpha, beta, cen_a, cen_b)
-
-    term1 = all_cases(power_a, power_b, alpha, beta, cen_a, cen_b, term1)
-    return term1
-
 print("Max exponent: ", xp.max(exponents_total))
 print("Min exponent: ", xp.min(exponents_total))
 print("Max position: ", xp.max(centers_total))
@@ -183,28 +143,6 @@ print("Min power: ", xp.min(powers), "\n")
 normals = normal(exponents_total, powers)
 normals = xp.outer(normals, normals)
 mult_coeffs = xp.outer(coefficients_total, coefficients_total)
-
-exp1, exp2 = xp.meshgrid(exponents_total, exponents_total)
-shape = centers_total.shape
-c1 = xp.broadcast_to(centers_total[None, :, :], (shape[0], shape[0], shape[1]))
-c2 = xp.broadcast_to(centers_total[:, None, :], (shape[0], shape[0], shape[1]))
-shape = powers.shape
-p1 = xp.broadcast_to(powers[None, :, :], (shape[0], shape[0], shape[1]))
-p2 = xp.broadcast_to(powers[:, None, :], (shape[0], shape[0], shape[1]))
-
-overlap = normals*mult_coeffs*total_overlap(exp1, exp2, c1, c2, p1, p2)
-overlap = overlap.reshape(exponents_total.shape[0], exp_shape[0], exp_shape[1])
-overlap = xp.sum(overlap, axis=-1)
-overlap = overlap.T
-overlap = overlap.reshape(-1, exp_shape[0], exp_shape[1])
-overlap = xp.sum(overlap, axis=-1)
-overlap_exp = overlap.T
-
-print("Condition Number: ", xp.linalg.cond(overlap_exp))
-print(xp.max(xp.abs(overlap_exp - overlap_exp.T)))
-eigenvalues = xp.linalg.eigvalsh(overlap_exp)
-print(eigenvalues)
-print(xp.diag(overlap_exp), "\n\n")
 
 overlap1 = overlap_1d(exponents_total, exponents_total, centers_total[:, 0], centers_total[:, 0], powers[:, 0], powers[:, 0])
 overlap2 = overlap_1d(exponents_total, exponents_total, centers_total[:, 1], centers_total[:, 1], powers[:, 1], powers[:, 1])
@@ -222,8 +160,4 @@ print(xp.max(xp.abs(overlap_true - overlap_true.T)))
 eigenvalues = xp.linalg.eigvalsh(overlap_true)
 print(eigenvalues)
 print(xp.diag(overlap_true), "\n")
-
-print("Max Absolute Difference: ", xp.max(xp.abs(overlap_true - overlap_exp)))
-avg = (overlap_true + overlap_exp)/2 + 1e-50
-print("Max Relative Difference: ", xp.max(xp.abs((overlap_true - overlap_exp)/avg)))
 
